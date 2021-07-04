41999939e7bdffa400f097a186f8dbdb2269e128
arundev1 <43666238+arundev1@users.noreply.github.com>
2021-07-04 23:26:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,11 +22,16 @@ function iterateTableData() {
     var checks = document.getElementsByName('food[]');
     var len = checks.length;
     let selectedFood = [];
+    var leng=selectedFood.length;
     for (var i=0; i<len; i++) {
         if(checks[i].checked){
             selectedFood.push(checks[i].value);
         }
     }
+    for (var j=0;j<leng;j++){
+        if(leng<2){
+            alert("Select 2 items")
+        }
     formValues.push({
         firstName: Form_val.firstName.value,
         lastName: Form_val.lastName.value,
