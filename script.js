7b21de73c3d0871ea0006349d5a2ff89c9b81bc8
arundev1 <43666238+arundev1@users.noreply.github.com>
2021-07-04 14:50:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var forms = document.getElementsByClassName('val');
+var form = document.getElementsByClassName('val');
     var validation = Array.prototype.filter.call(forms, function(form) {
     form.addEventListener('submit', function(event) {
         event.preventDefault();
@@ -9,32 +9,31 @@ var forms = document.getElementsByClassName('val');
             console.log(forms);
             iterateTableData();
         }
-        form.classList.add('was-validated');
+        form.classList.add('validation_done');
     }, false);
     });
 
   
-var myForm = document.getElementById('myForm');
+var form_val = document.getElementById('form_val');
 let formValues = [];
 
 function iterateTableData() {
     var cboxes = document.getElementsByName('food[]');
-    var len = cboxes.length;
     let selectedFood = [];
-    for (var i=0; i<len; i++) {
+    for (var i=0; i<cboxes.length; i++) {
         if(cboxes[i].checked){
             selectedFood.push(cboxes[i].value);
         }
     }
     formValues.push({
-        firstName: myForm.firstName.value,
-        lastName: myForm.lastName.value,
-        address: myForm.address.value,
-        pincode: myForm.pincode.value,
-        gender: myForm.gender.value,
+        firstName: form_val.firstName.value,
+        lastName: form_val.lastName.value,
+        address: form_val.address.value,
+        pincode: form_val.pincode.value,
+        gender: form_val.gender.value,
         food: selectedFood,
-        country: myForm.country.value,
-        state: myForm.state.value
+        country: form_val.country.value,
+        state: form_val.state.value
     })
     var tbody = '';
     formValues.forEach(value=>{
@@ -50,6 +49,6 @@ function iterateTableData() {
     });
     var tableData = document.getElementById('table-data');
     tableData.innerHTML = tbody;
-    myForm.reset();
-    forms.classList.remove('was-validated');
+    form_val.reset();
+    forms.classList.remove('validation_done');
 }
